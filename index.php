e53f6b9679d3a542d240b04e3e285e56995c666e
Kenny GAUDRY <Kenny.gaudry@hotmail.fr>
2023-10-26 14:38:48 +0200

    création variables,Concatenation

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+    // $test = "coucou";
+    // echo $test.'<br>';
+
+    // $nombre = 5;
+    // echo $nombre;
+
+    // $tab = array(5,"test");
+    // echo $tab;
+
+    $prenom = "Kenny";
+    $nom = "GAUDRY";
+    
+    echo $nom." ".$prenom;
+
+?> 
